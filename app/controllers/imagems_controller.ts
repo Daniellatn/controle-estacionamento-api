9b7fbb712cea557b65b5750bf2fd3b12125732ca
Daniella Nascimento <daniellateun@gmail.com>
2024-06-05 13:18:13 -0300

    Upload e Download de imagens

--- app/controllers/imagems_controller.ts
+++ app/controllers/imagems_controller.ts
@@ -1,6 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import app from '@adonisjs/core/services/app'
 
 import Imagem from "#models/imagem";
+import { normalize } from 'node:path';
 
 export default class ImagemsController {
   async index({request}: HttpContext) {
@@ -9,23 +11,23 @@ export default class ImagemsController {
                         .paginate(page, 5)
   }
 
-  async show({params}: HttpContext) {
-    try {
-      return await Imagem.findOrFail(params.id)
-    } catch (error) {
-      return {message: 'Registro não encontrado.'}
-    }
-    // return await Bloco.query()
-    //                     .where('id', params.id)
-    //                     .preload('type')
-    //                     .preload('ingredients')
-    //                     .first()
-  }
+  // async show({params}: HttpContext) {
+  //   try {
+  //     return await Imagem.findOrFail(params.id)
+  //   } catch (error) {
+  //     return {message: 'Registro não encontrado.'}
+  //   }
+  //   // return await Bloco.query()
+  //   //                     .where('id', params.id)
+  //   //                     .preload('type')
+  //   //                     .preload('ingredients')
+  //   //                     .first()
+  // }
 
-  async store({request}: HttpContext) {
-    const data = request.only(['nome', 'tipo', 'imagem'])
-    return await Imagem.create(data)
-  }
+  // async store({request}: HttpContext) {
+  //   const data = request.only(['nome', 'tipo', 'imagem'])
+  //   return await Imagem.create(data)
+  // }
 
   async update({params, request}: HttpContext) {
     const imagem = await Imagem.findOrFail(params.id)
@@ -49,5 +51,46 @@ export default class ImagemsController {
     } catch (error) {
       return {message: error}
     }
+  }
+
+  //Upload
+  async store({request, response}: HttpContext) {
+    console.log('RRR: ', request)
+    const img = request.file('imagem', {
+      size: '2mb',
+      extnames: ['jpg', 'png', 'jpeg']
+    })
+
+    if (!img?.isValid) {
+      return response.badRequest({
+        errors: img?.errors
+      })
+    }
+    const nome = img.clientName.split('.')[0];
+    await img.move(app.makePath('uploads'), {
+      name: `${nome}.${img.extname}`
+    })
+    const data = {
+      nome: nome,
+      tipo: img.extname,
+      imagem: img.clientName
+    }
+    console.log('DDD: ', data)
+    return await Imagem.create(data)
+  }
+  // Download
+  async show({params, response}: HttpContext) {
+    const PATH_TRAVERSAL_REGEX = /(?:^|[\\/])\.\.(?:[\\/]|$)/
+
+    const dados = await Imagem.findOrFail(params.id)
+    const nomeImagem = normalize(dados.imagem)
+
+    if (PATH_TRAVERSAL_REGEX.test(nomeImagem)) {
+      return response.badRequest('Caminho incorreto.')
+    }
+
+    const caminhoImagem = app.makePath('uploads', nomeImagem)
+
+    return response.download(caminhoImagem)
   }
 }
